4391ded1f5bbe0e65ba2aa8e42500059416532c5
Askthemaster100 <t0527168987@gmail.com>
2025-02-27 21:09:07 +0200

    creating home-page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { AuthComponent } from './components/auth/auth.component';
+import { CommonModule } from '@angular/common';
+import { HomePageComponent } from "./components/home-page/home-page.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/home-page/home-page.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  templateUrl:'./home-page.component.html',
+  styleUrls: ['./home-page.component.scss'],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, MatGridListModule]
+})
+export class HomePageComponent {
+  images = [
+    '/images/d1.jpg',
+    '/images/d2.jpg',
+    '/images/d3.jpg',
+    '/images/d4.jpg',
+    '/images/d5.jpg',
+    '/images/d6.jpg',
+  ];
+  
+  
+  currentIndex = 0;
+
+  nextImage() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevImage() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+}
+
