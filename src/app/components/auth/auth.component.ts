3344e009af96f0f3a1b149f706572fb8fe1e5d93
Askthemaster100 <t0527168987@gmail.com>
2025-03-10 03:14:24 +0200

    add role selector,alon in page

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,6 +1,5 @@
-
-import { Component, Inject } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -8,24 +7,32 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { AuthService} from '../../../services/auth-service'; 
+import { AuthService } from '../../../services/auth-service';
 import { HttpClientModule } from '@angular/common/http';
+import { MatOption } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-auth',
-  standalone: true, 
-  templateUrl: './auth.component.html', 
+  standalone: true,
+  templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
-  imports: [ 
+  imports: [
     CommonModule,
     ReactiveFormsModule,
     MatInputModule,
     MatButtonModule,
     MatCardModule,
     MatSnackBarModule,
-    HttpClientModule
+    HttpClientModule,
+    MatOption,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule
   ],
-  // providers: [ AuthService]
 })
 export class AuthComponent {
   authForm: FormGroup;
@@ -39,6 +46,7 @@ export class AuthComponent {
       name: [{ value: '', disabled: this.isLoginMode }, Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
+      role: [null, this.isLoginMode ? [] : [Validators.required]]
     });
   }
 
@@ -55,30 +63,33 @@ export class AuthComponent {
     if (this.authForm.invalid) {
       return;
     }
-  
-    const { name, email, password } = this.authForm.value;
-  
+    const { name, email, password, role } = this.authForm.value;
+
     try {
       if (this.isLoginMode) {
         console.log("in login mode");
-        
+
         this.authService.login(email, password).subscribe(
-          (res: any) => {
-            console.log("in login app",res);
-        });
-        this.snackBar.open('Login successful!', 'Close', { duration: 3000 });
+          (res) => {
+            console.log("in login app", res);
+            if (res.userId)
+              this.snackBar.open("Login successful!", 'Close', { duration: 3000 });
+            else
+              this.snackBar.open("Login failed!", 'Close', { duration: 3000 });
+          });
       } else {
-        this.authService.register(name, email, password, 'student').subscribe(
+        console.log("the role is",role);
+        this.authService.register(name, email, password, role).subscribe(
           (res: any) => {
-            console.log("in register app",res);
-        }
+            console.log("in register app", res);
+            this.snackBar.open(res.message, 'Close', { duration: 3000 });
+          }
         );
-        this.snackBar.open('Registration successful!', 'Close', { duration: 3000 });
         this.onSwitchMode();
       }
       this.authForm.reset();
     } catch (error) {
-      this.snackBar.open('Authentication failed. Please try again.', 'Close', { duration: 3000 });
+      this.snackBar.open('Authentication failed. Please try again.', 'Close', { duration: 3500 });
     }
   }
 }
