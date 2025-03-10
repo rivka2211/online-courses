e31ead2c335612657ebf0b848ae8d98a565a9b88
Askthemaster100 <t0527168987@gmail.com>
2025-03-10 03:21:26 +0200

    routing and design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AuthComponent } from './components/auth/auth.component';
 import { CommonModule } from '@angular/common';
-import { HomePageComponent } from "./components/home-page/home-page.component";
-import { CoursesComponent } from "./components/courses/courses.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomePageComponent, AuthComponent, CoursesComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,17 +4,18 @@ import { AuthComponent } from './components/auth/auth.component';
 import { CoursesComponent } from './components/courses/courses.component';
 import { LessonDetailsComponent } from './components/lesson-details/lesson-details.component';
 import { CourseDetailComponent } from './components/course-details/course-details.component';
-import { AuthGuard } from '../services/AuthGuard';
+import { AuthGuard } from '../guards/auth-guard';
+import { TeacherGuard } from '../guards/teacher-guard';
+
 
 export const routes: Routes = [
-
-    // { path: '', component: HomePageComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'auth', component: AuthComponent },
     { path: 'home', component: HomePageComponent, canActivate: [AuthGuard] },
     { path: 'courses', component: CoursesComponent, canActivate: [AuthGuard] },
     { path: 'courses/:courseId', component: CourseDetailComponent, canActivate: [AuthGuard] },
     { path: 'courses/:courseId/lessons/:lessonId', component: LessonDetailsComponent, canActivate: [AuthGuard] },
+    { path: 'manage', component: CoursesComponent, canActivate: [TeacherGuard] },
     { path: '**', redirectTo: '/home' }
 ];
 
