d4c7bbc1615dfd3582df294c08eedcb0e7d7d3e5
Askthemaster100 <t0527168987@gmail.com>
2025-03-10 03:18:55 +0200

    something

--- src/app/components/course-details/course-details.component.ts
+++ src/app/components/course-details/course-details.component.ts
@@ -9,39 +9,34 @@ import { UserService } from '../../../services/user-service';
 
 @Component({
   selector: 'app-course-details',
-  templateUrl:'./course-details.component.html',
+  templateUrl: './course-details.component.html',
   styleUrls: ['./course-details.component.css']
 })
 export class CourseDetailComponent implements OnInit {
   @Input()
-  course!:Course
-  isTeacher=false
-  lessons:Lesson[]=[];
-  teacherName: string='';
+  course!: Course
+  isTeacher = false
+  lessons: Lesson[] = [];
+  teacherName: string = '';
   constructor(
     private route: ActivatedRoute,
     private lessonService: LessonService,
     public dialog: MatDialog,
     private userService: UserService
   ) { }
-  
-  
+
+
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
-      const id = params.get('productId');
-      if (id) {
-        this.course.id =parseInt(id);
-      } else {
-        console.error('Product ID not found');
-      }}
-    )
-    this.getLessons();
-    this.isTeacher=localStorage.getItem('role')=='teacher'
-    this.teacherName=this.userService.getUserName(this.course.teacherId);
+      const id = params.get('courseId');
+      this.getLessons(parseInt(id || "0"))
+    },);
+    this.isTeacher = localStorage.getItem('role') == 'teacher'
+    this.teacherName = this.userService.getUserName(this.course.teacherId);
   }
-  
-  getLessons(): void {
-    this.lessonService.getLessons(this.course.id).subscribe(
+
+  getLessons(courseId: number = this.course.id): void {
+    this.lessonService.getLessons(courseId).subscribe(
       (data) => {
         this.lessons = data;
       },
@@ -51,13 +46,13 @@ export class CourseDetailComponent implements OnInit {
     );
   }
   addLesson() {
-   alert("add lesson")
+    alert("add lesson")
   }
   editLesson(lessonId: number): void {
     alert("עריכת שיעור")
     // פעולה לעריכת שיעור
   }
-  
+
   deleteLesson(lessonId: number): void {
     this.lessonService.deleteLesson(this.course.id, lessonId).subscribe(
       () => {
@@ -68,15 +63,15 @@ export class CourseDetailComponent implements OnInit {
       }
     );
   }
-  openLessonDialog(lessonId: number, courseName: string, teacherName: string,courseId: number): void {
+  openLessonDialog(lessonId: number): void {
     const dialogRef = this.dialog.open(LessonDetailsComponent, {
       width: '500px',
-      data: { lessonId, courseName, teacherName,courseId }
+      data: { lessonId, courseId: this.course.id, courseName: this.course.title, teacherNmae: this.teacherName }
     });
-  
+
     dialogRef.afterClosed().subscribe(() => {
       console.log('Lesson dialog closed');
     });
-  } 
+  }
 }
 
